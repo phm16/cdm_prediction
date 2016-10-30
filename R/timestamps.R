ea4a91f26d75fb18f35bf6c22ec29bbecfc3e4b5
zsharpm <zachmsharp@gmail.com>
2016-10-30 12:58:42 -0500

    script to look at the intervals between timestamps

--- R/timestamps.R
+++ R/timestamps.R
@@ -1,9 +1,15 @@
 
 
+# timestamps per wafer / stage
+base_df %>% count(WAFER_ID, STAGE) %>%
+  ggplot(., aes(x = n)) +
+  geom_histogram() +
+  lims(x = c(0, 500))
+
 # show the distribution of the duration between timestamps
 
 # max 10 gap
-train_df %>% select(TIMESTAMP) %>%
+base_df %>% select(TIMESTAMP) %>%
   arrange(TIMESTAMP) %>%
   mutate(PREV_TIMESTAMP = lag(TIMESTAMP, 1),
          DIFF = TIMESTAMP - PREV_TIMESTAMP) %>%
@@ -11,15 +17,15 @@ train_df %>% select(TIMESTAMP) %>%
   ggplot(., aes(x = DIFF)) + geom_histogram()
 
 # max 1 gap
-train_df %>% select(TIMESTAMP) %>%
+base_df %>% select(TIMESTAMP, STATION) %>%
   arrange(TIMESTAMP) %>%
   mutate(PREV_TIMESTAMP = lag(TIMESTAMP, 1),
          DIFF = TIMESTAMP - PREV_TIMESTAMP) %>%
   filter(DIFF < 10, DIFF > -10) %>%
-  ggplot(., aes(x = DIFF)) + geom_histogram()
+  ggplot(., aes(x = DIFF, fill = STATION)) + geom_histogram(position = "dodge")
 
 # time series
-train_df %>% select(TIMESTAMP, STAGE, CHAMBER) %>%
+base_df %>% select(TIMESTAMP, STAGE, CHAMBER) %>%
   unite(STAGE_CHAMBER, STAGE, CHAMBER) %>%
   arrange(TIMESTAMP) %>%
   mutate(PREV_TIMESTAMP = lag(TIMESTAMP, 1),
