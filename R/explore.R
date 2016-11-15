bbf84e41f3ef47083083a5aa25f441ab6732169c
zsharpm <zachmsharp@gmail.com>
2016-11-14 21:58:31 -0600

    update object names

--- R/explore.R
+++ R/explore.R
@@ -8,9 +8,9 @@
 # usage of backing film (max) good predictor for outliers 
 # max timestamp:  487268210
 # min timestamp:  481634410
-# 70% timestamp:  485578070 min(base_df$TIMESTAMP) + 0.7 * (max(base_df$TIMESTAMP) - min(base_df$TIMESTAMP))
+# 70% timestamp:  485578070 min(train_df_ts$TIMESTAMP) + 0.7 * (max(train_df_ts$TIMESTAMP) - min(train_df_ts$TIMESTAMP))
 
-glimpse(base_df)
+glimpse(train_df_ts)
 
 # Observations: 672,744
 # Variables: 26
@@ -43,8 +43,9 @@ glimpse(base_df)
 
 # long data frame ---------------------------------------------------------
 
-base_df_long <- base_df %>% gather(var, val, -WAFER_ID, -STAGE, -STATION, -TIMESTAMP, -CHAMBER) %>%
-  filter(!var %in% c("MACHINE_ID", "MACHINE_DATA"))
+train_df_ts_long <- train_df_ts %>% 
+  select(-MACHINE_ID, -MACHINE_DATA) %>%
+  gather(var, val, -WAFER_ID, -STAGE, -POLISH_TYPE, -POLISH_PHASE, -STATION, -TIMESTAMP, -CHAMBER)
 
 # response ----------------------------------------------------------------
 
@@ -66,17 +67,17 @@ ggplot(response %>%
 # training observations count ---------------------------------------------
 
 # check the number of observations per job
-base_df %>% 
+train_df_ts %>% 
   count(WAFER_ID, STAGE) %>% 
   ggplot(., aes(x = n)) + geom_histogram()
 
 # there are some jobs with significantly more data than others
-base_df %>%
+train_df_ts %>%
   count(WAFER_ID, STAGE) %>% 
   filter(n < 1000) %>%
   ggplot(., aes(x = n)) + geom_histogram()
 
-base_df %>% 
+train_df_ts %>% 
   count(WAFER_ID, STAGE) %>% 
   .$n %>%
   quantile(.)
@@ -85,7 +86,7 @@ base_df %>%
 # time series plots -------------------------------------------------------
 
 # avg removal rate vs. time
-ggplot(base_df, 
+ggplot(train_df_ts, 
        aes(
          x = TIMESTAMP,
          y = AVG_REMOVAL_RATE
@@ -93,7 +94,7 @@ ggplot(base_df,
   geom_line()
 
 # exclude the outliers
-ggplot(base_df %>% 
+ggplot(train_df_ts %>% 
          filter(AVG_REMOVAL_RATE < 1000), 
        aes(
          x = TIMESTAMP,
@@ -102,10 +103,49 @@ ggplot(base_df %>%
        )) + 
   geom_point(alpha = 0.5)
 
+# avg removal rate vs. time
+ggplot(train_df_ts %>% 
+         filter(AVG_REMOVAL_RATE < 4000) %>%
+         select(TIMESTAMP, STATION, USAGE_OF_DRESSER, AVG_REMOVAL_RATE) %>%
+         slice(seq(1, nrow(train_df_ts), 100)) %>%
+         group_by(STATION) %>%
+         mutate(USAGE_OF_DRESSER_scaled = scale(USAGE_OF_DRESSER),
+                AVG_REMOVAL_RATE_scaled = scale(AVG_REMOVAL_RATE)) %>%
+         ungroup() %>%
+         select(-USAGE_OF_DRESSER, -AVG_REMOVAL_RATE) %>%
+         gather(VAR, VAL, -TIMESTAMP, -STATION),
+       aes(
+         x = TIMESTAMP,
+         y = VAL,
+         color = VAR
+       )) + 
+  geom_line() +
+  facet_wrap(~ STATION) +
+  geom_smooth(se = FALSE) +
+  geom_vline(xintercept = 481731974, linetype = "dashed") +
+  geom_vline(xintercept = 482953354, linetype = "dashed") +
+  geom_vline(xintercept = 484369734, linetype = "dashed") +
+  geom_vline(xintercept = 485769885, linetype = "dashed") +
+  geom_vline(xintercept = 487184758, linetype = "dashed")
+
+
+ggplot(train_df_ts %>% 
+         filter(AVG_REMOVAL_RATE < 4000) %>%
+         select(TIMESTAMP, STATION, USAGE_OF_DRESSER, AVG_REMOVAL_RATE) %>%
+         group_by(STATION) %>%
+         mutate(USAGE_OF_DRESSER_scaled = scale(USAGE_OF_DRESSER),
+                AVG_REMOVAL_RATE_scaled = scale(1 / AVG_REMOVAL_RATE)),
+       aes(
+         x = USAGE_OF_DRESSER_scaled,
+         y = AVG_REMOVAL_RATE_scaled
+       )) + 
+  geom_point(alpha = 0.5) +
+  facet_wrap(~ STATION)
+
 
 # single series -----------------------------------------------------------
 
-ggplot(base_df %>% 
+ggplot(train_df_ts %>% 
          filter(WAFER_ID == 371447024, STAGE == "A"),
        aes(x = TIMESTAMP, y = CENTER_AIR_BAG_PRESSURE, color = as.factor(CHAMBER))) +
   geom_line() +
@@ -123,10 +163,10 @@ stage_chamber_durations %>%
   geom_boxplot()
 
 # quantiles
-quantile(base_df$TIMESTAMP, probs = seq(0, 1, 0.1))
+quantile(train_df_ts$TIMESTAMP, probs = seq(0, 1, 0.1))
 
 # look at subset of time series - facets
-ggplot(base_df %>% 
+ggplot(train_df_ts %>% 
          filter(AVG_REMOVAL_RATE < 1000, TIMESTAMP <= 481985972),
        aes(
          x = TIMESTAMP,
@@ -138,7 +178,7 @@ ggplot(base_df %>%
   geom_smooth()
 
 # look at subset of time series - colors
-ggplot(base_df %>% 
+ggplot(train_df_ts %>% 
          filter(AVG_REMOVAL_RATE < 1000, TIMESTAMP <= 481985972) %>%
          mutate(STAGE_CHAMBER = paste0(STAGE, "_", CHAMBER)),
        aes(
@@ -149,7 +189,7 @@ ggplot(base_df %>%
   geom_point(alpha = 0.5) +
   geom_smooth()
 
-ggplot(base_df %>%
+ggplot(train_df_ts %>%
          unite(STAGE_CHAMBER, STAGE, CHAMBER) %>%
          filter(WAFER_ID == -4230160598),
        aes(
@@ -161,7 +201,7 @@ ggplot(base_df %>%
 
 # aggregate time series ---------------------------------------------------
 
-base_df_agrgt <- base_df %>%
+base_df_agrgt <- train_df_ts %>%
   mutate(TIMESTAMP2 = floor(TIMESTAMP / 1000) * 1000) %>%
   select_if(is.numeric) %>%
   group_by(TIMESTAMP2) %>%
@@ -178,19 +218,26 @@ ggplot(base_df_agrgt,
 
 # job plots ---------------------------------------------------------------
 
-ggplot(base_df_long %>% filter(WAFER_ID == 371447024, STAGE == "A"),
+ggplot(train_df_ts_long %>% filter(WAFER_ID == 371447024, STAGE == "A"),
+       aes(x = TIMESTAMP, y = val)) +
+  geom_line() +
+  facet_wrap(~ var, scales = "free") +
+  theme_grey(8) +
+  ggsave("figures/371447024_A.png", device = "png", width = 8, height = 6)
+
+ggplot(train_df_ts_long %>% filter(WAFER_ID == 371447024, STAGE == "A"),
        aes(x = TIMESTAMP, y = val, color = as.factor(CHAMBER))) +
   geom_line() +
   facet_wrap(~ var, scales = "free")
 
-ggplot(base_df_long %>% filter(WAFER_ID == -4019511766),
+ggplot(train_df_ts_long %>% filter(WAFER_ID == -4019511766),
        aes(x = TIMESTAMP, y = val, color = as.factor(CHAMBER))) +
   geom_line() +
   facet_wrap(~ var, scales = "free")
 
 wafer_ids <- unique(response$WAFER_ID)
 
-ggplot(base_df_long %>% filter(WAFER_ID == 2939014292, STAGE == "A"),
+ggplot(train_df_ts_long %>% filter(WAFER_ID == 2939014292, STAGE == "A"),
        aes(x = TIMESTAMP, y = val, color = as.factor(CHAMBER))) +
   geom_line() +
   facet_wrap(~ var, scales = "free") +
@@ -201,7 +248,7 @@ lapply(wafer_ids[1:10], function(wafer_id) {
 
   title <- wafer_id
   
-  ggplot(base_df_long %>% filter(WAFER_ID == wafer_id),
+  ggplot(train_df_ts_long %>% filter(WAFER_ID == wafer_id),
          aes(x = TIMESTAMP, y = val, color = STAGE)) +
     geom_line() +
     facet_wrap(~ var, scales = "free") +
@@ -213,34 +260,34 @@ lapply(wafer_ids[1:10], function(wafer_id) {
 
 # usage -------------------------------------------------------------------
 
-ggplot(base_df, 
+ggplot(train_df_ts, 
        aes(x = TIMESTAMP, y = USAGE_OF_BACKING_FILM)) + 
   geom_line()
 
-ggplot(base_df, 
+ggplot(train_df_ts, 
        aes(x = TIMESTAMP, y = USAGE_OF_DRESSER)) + 
   geom_line()
 
-ggplot(base_df, 
+ggplot(train_df_ts, 
        aes(x = TIMESTAMP, y = USAGE_OF_POLISHING_TABLE)) + 
   geom_line()
 
-ggplot(base_df, 
+ggplot(train_df_ts, 
        aes(x = TIMESTAMP, y = USAGE_OF_DRESSER_TABLE)) + 
   geom_line()
 
-ggplot(base_df, 
+ggplot(train_df_ts, 
        aes(x = TIMESTAMP, y = USAGE_OF_MEMBRANE)) + 
   geom_line()
 
-ggplot(base_df, 
+ggplot(train_df_ts, 
        aes(x = TIMESTAMP, y = USAGE_OF_PRESSURIZED_SHEET)) + 
   geom_line()
 
 
 # individual variables ----------------------------------------------------
 
-ggplot(base_df %>% filter(WAFER_ID == 371447024, STAGE == "A"), 
+ggplot(train_df_ts %>% filter(WAFER_ID == 371447024, STAGE == "A"), 
        aes(x = TIMESTAMP, y = RETAINER_RING_PRESSURE, color = as.character(CHAMBER))) + 
   geom_line()
 
@@ -248,7 +295,7 @@ ggplot(base_df %>% filter(WAFER_ID == 371447024, STAGE == "A"),
 # center air bag pressure -------------------------------------------------
 
 ggplot(
-  base_df %>%
+  train_df_ts %>%
     filter(AVG_REMOVAL_RATE < 1000),
   aes(x = CENTER_AIR_BAG_PRESSURE_max, y = AVG_REMOVAL_RATE)) + 
   geom_point() +
@@ -258,7 +305,7 @@ ggplot(
 
 # rework ------------------------------------------------------------------
 
-ggplot(base_df %>% filter(WAFER_ID == -4019511766),
+ggplot(train_df_ts %>% filter(WAFER_ID == -4019511766),
        aes(x = TIMESTAMP, y = CHAMBER, color = STAGE)) +
   geom_point() +
   geom_vline(xintercept = 483966563)
@@ -266,9 +313,65 @@ ggplot(base_df %>% filter(WAFER_ID == -4019511766),
 
 # facet plots -------------------------------------------------------------
 
-ggplot(base_df_long %>%
+ggplot(train_df_ts_long %>%
          filter(!substr(var, 1, 5) == "USAGE"),
        aes(x = val)) +
   geom_histogram() +
   facet_wrap( ~ var, scales = "free") +
   theme_grey(8)
+
+
+# missing data ------------------------------------------------------------
+
+ggplot(train_df_ts %>%
+         filter(WAFER_ID == -4224160600, STATION == "A456"), 
+       aes(x = TIMESTAMP, y = SLURRY_FLOW_LINE_A, color = as.factor(CHAMBER))) + 
+  geom_line()
+
+AVG_REMOVAL_RATE_lag1 = lag(AVG_REMOVAL_RATE, 1L, default = 0L)
+
+ggplot(train_df_ts %>%
+         #filter(WAFER_ID == -4224160600, STATION == "A456"),
+         filter(WAFER_ID == 371447024, STAGE == "A"),
+       aes(x = TIMESTAMP, y = CENTER_AIR_BAG_PRESSURE, color = as.factor(CHAMBER))) + 
+  geom_line()
+
+ggplot(train_df_ts %>%
+         #filter(WAFER_ID == -4224160600, STATION == "A456"),
+         filter(WAFER_ID == 371447024, STAGE == "A"),
+       aes(x = TIMESTAMP, y = SLURRY_FLOW_LINE_A, color = as.factor(CHAMBER))) + 
+  geom_line()
+
+ggplot(train_df_ts %>%
+         #filter(WAFER_ID == -4224160600, STATION == "A456"),
+         filter(WAFER_ID == 371447024, STAGE == "A"),
+       aes(x = TIMESTAMP, y = USAGE_OF_DRESSER, color = as.factor(CHAMBER))) + 
+  geom_line()
+
+ggplot(train_df_ts %>%
+         #filter(WAFER_ID == -4224160600, STATION == "A456"),
+         filter(WAFER_ID == 371447024, STAGE == "A"),
+       aes(x = TIMESTAMP, y = USAGE_OF_MEMBRANE, color = as.factor(CHAMBER))) + 
+  geom_line()
+
+
+# multiple vars one plot --------------------------------------------------
+
+ggplot(train_df_ts_long %>%
+         filter(WAFER_ID == -4224160600, STAGE == "A", CHAMBER == 4L) %>%
+         filter(var %in% c(
+           "AVG_REMOVAL_RATE",
+           "CENTER_AIR_BAG_PRESSURE",
+           "SLURRY_FLOW_LINE_A",
+           "SLURRY_FLOW_LINE_B",
+           "SLURRY_FLOW_LINE_C",
+           "WAFER_ROTATION",
+           "STAGE_ROTATION"
+         )) %>%
+         group_by(var) %>%
+         mutate(scaled_val = scale(val)) %>%
+         ungroup(), 
+       aes(x = TIMESTAMP, y = scaled_val, color = var)) +
+  geom_line() +
+  facet_wrap(~ CHAMBER)
+
