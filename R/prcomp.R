af834942e199a8e26ab104c0d6cd62042bea57db
zsharpm <zachmsharp@gmail.com>
2016-11-14 22:16:51 -0600

    move to misc folder

--- R/prcomp.R
+++
@@ -1,9 +0,0 @@
-# principal components ----------------------------------------------------
-
-pressure_prcomp <- mdl_df %>% select(contains("PRESSURE")) %>% 
-  mutate_all(.funs = scale) %>% 
-  prcomp(.) %>%
-  predict(., mdl_df) %>%
-  data.frame(.)
-
-mdl_df_prcomp <- mdl_df %>% bind_cols(pressure_prcomp)
